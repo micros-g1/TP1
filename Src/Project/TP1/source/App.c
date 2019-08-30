05acf12358e948ff10a54f2dc61564a081e1dc0b
taomasgonzalez <togonzalez@itba.edu.ar>
2019-08-30 17:37:03 -0300

    Subido version compilable para iniciar tp

--- Src/Project/TP1/source/App.c
+++ Src/Project/TP1/source/App.c
@@ -27,10 +27,7 @@
 /*******************************************************************************
  * FUNCTION PROTOTYPES FOR PRIVATE FUNCTIONS WITH FILE LEVEL SCOPE
  ******************************************************************************/
-void button_handler(void);
 void systick_callback(void);
-
-bool baliza;
 
 /*******************************************************************************
  *******************************************************************************
@@ -41,12 +38,9 @@ bool baliza;
 /* Función que se llama 1 vez, al comienzo del programa */
 void App_Init (void)
 {
-	baliza = false;
 	interrupts_init();
 	gpioMode(PIN_LED_BLUE,OUTPUT);
-    gpioWrite(PIN_LED_BLUE, HIGH);
     gpioMode(PIN_SW3, INPUT);
-    gpioIRQ(PIN_SW3, GPIO_IRQ_MODE_FALLING_EDGE, &button_handler);
     SysTick_Init(systick_callback);
 }
 
@@ -63,28 +57,10 @@ void App_Run (void)
  *******************************************************************************
  ******************************************************************************/
 
-void button_handler(void){
-	baliza = !baliza;
-	if(!baliza){
-		gpioWrite(PIN_LED_BLUE, HIGH);
-	}
-}
-
 void systick_callback(void)
 {
-	static uint32_t counter = 0;
 
-	// use counter for dividing systick frequency
-	if(baliza){
-		if (counter == SYSTICK_ISR_FREQUENCY_HZ/BLINK_FREQ_HZ/2) {
-			// toggle pin twice per period
-			gpioToggle(PIN_LED_BLUE);
-			counter = 0;
-		}
-		else {
-			counter++;
-		}
-	}
+
 
 }
 
