cdbc178d9b5fff9c4bcac3db48e9dbc0d9208ed0
taomasgonzalez <togonzalez@itba.edu.ar>
2019-09-03 00:46:02 -0300

    levante al nuevo Systick

--- Src/Project/TP1/source/SysTick.c
+++ Src/Project/TP1/source/SysTick.c
@@ -20,33 +20,80 @@
 #endif /* FCLK % SYSTICK_ISR_FREQUENCY_HZ != 0 */
 
 
-typedef void (*systick_callback_t)(void);
 systick_callback_t callback;
 
+typedef struct {
+    systick_callback_t func;
+    unsigned int counter;
+    unsigned int reload;
+    bool enabled;
+} st_cb_data_t;
 
+static st_cb_data_t st_callbacks[MAX_N_ST_CALLBACKS];
 
 void SysTick_Handler(void);
 
-bool SysTick_Init (void (*funcallback)(void))
+
+void systick_init ()
 {
 	NVIC_EnableIRQ(SysTick_IRQn);
-	if (callback == NULL) {
-		SysTick->CTRL = 0x00; 								// enable systick interrupts
-		SysTick->LOAD = FCLK/SYSTICK_ISR_FREQUENCY_HZ - 1; 	// load value = pulses per period - 1
-		SysTick->VAL=0x00;
-		SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk| SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
-		callback = funcallback;								// set ISR
+	SysTick->CTRL = 0x00; 								// enable systick interrupts
+	SysTick->LOAD = FCLK/SYSTICK_ISR_FREQUENCY_HZ - 1; 	// load value = pulses per period - 1
+	SysTick->VAL=0x00;
+	SysTick->CTRL = SysTick_CTRL_CLKSOURCE_Msk| SysTick_CTRL_TICKINT_Msk | SysTick_CTRL_ENABLE_Msk;
 
-		return true;
-	}
-	else {
-		return false;
+	unsigned int i = 0;
+	for (i = 0; i < MAX_N_ST_CALLBACKS; i++) {
+	    st_callbacks[i].func = NULL;
 	}
 }
 
-void SysTick_Handler(void)
+void SysTick_Handler(void) // DO NOT CHANGE THE NAME, overrides core_cm4.h weak definition
 {
 	// for SysTick, clearing the interrupt flag is not necessary
 	// it is not an omission!
-	callback();
+    unsigned int i;
+    for (i = 0; i < MAX_N_ST_CALLBACKS; i++) {
+        if (st_callbacks[i].func != NULL && st_callbacks[i].enabled) {
+            st_callbacks[i].counter++;
+            if (st_callbacks[i].counter == st_callbacks[i].reload) {
+                st_callbacks[i].counter = 0;
+            }
+        }
+    }
+}
+
+
+
+unsigned int systick_add_callback(systick_callback_t cb, unsigned int reload)
+{
+    unsigned int id = MAX_N_ST_CALLBACKS;
+    if (cb != NULL) {
+        unsigned int i = 0;
+        while (i < MAX_N_ST_CALLBACKS && id == MAX_N_ST_CALLBACKS) {
+            if (st_callbacks[i].func == NULL) {
+                st_callbacks[i].func = cb;
+                st_callbacks[i].enabled = true;
+                st_callbacks[i].counter = 0;
+                st_callbacks[i].reload = reload;
+
+                id = i;
+            }
+            i++;
+        }
+    }
+
+    return id;
+}
+
+void systick_enable_callback(unsigned int id) {
+    if (id < MAX_N_ST_CALLBACKS) {
+        st_callbacks[id].enabled = true;
+    }
+}
+
+void systick_disable_callback(unsigned int id) {
+    if (id < MAX_N_ST_CALLBACKS) {
+        st_callbacks[id].enabled = false;
+    }
 }
