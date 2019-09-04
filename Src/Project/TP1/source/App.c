30af104a942c2e9c17def08d002128412f6cf4f3
Gonzalo Reina <greina@itba.edu.ar>
2019-09-04 15:11:51 -0300

    Rotary Encoder re-implemented, using callbacks now
    Rotary Encoder re-implemented
    Now it is a single .h file
    Event Queue deleted.

--- Src/Project/TP1/source/App.c
+++ Src/Project/TP1/source/App.c
@@ -13,6 +13,7 @@
 #include "gpio.h"
 #include "SysTick.h"
 
+#include "rotary_encoder.h"
 /*******************************************************************************
  * CONSTANT AND MACRO DEFINITIONS USING #DEFINE
  ******************************************************************************/
@@ -35,19 +36,32 @@ void clock_callback(void);
  *******************************************************************************
  ******************************************************************************/
 
+void rotary_encoder_callback(re_event_t ev);
+
 /* Función que se llama 1 vez, al comienzo del programa */
 void App_Init (void)
 {
 	interrupts_init();
+	systick_init();
+	rotary_encoder_init();
+	rotary_encoder_set_callback(rotary_encoder_callback);
 
 }
 
 /* Función que se llama constantemente en un ciclo infinito */
+
 void App_Run (void)
 {
 	while(1);
 }
 
+char* ev_name[] = {"RE_LL_LEFT","RE_LL_RIGHT","RE_LL_BUTTON_DOWN","RE_LL_BUTTON_UP","RE_LL_TIMEOUT","RE_LL_NO_EVENT","RE_LL_TOTAL_EVENTS"};
+
+
+void rotary_encoder_callback(re_event_t ev)
+{
+	//TODO: Test Rotary Encoder
+}
 
 /*******************************************************************************
  *******************************************************************************
